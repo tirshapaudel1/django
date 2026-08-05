96b86a2f9c3b61ac83dca175afe96e7d95e36e5d
tirshapaudel1 <tirshapaudel@gmail.com>
2026-08-05 21:46:08 +0545

    pages

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=54)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     slug = models.CharField(max_length=100, unique=True, blank=True)
     author = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    #category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def save(self, *args, **kwargs):
         if not self.slug:
-            base_slug =self.title.lower().replace(' ','-')
+            #base_slug =self.title.lower().replace(' ','-')
+            base_slug = slugify(self.title)
             slug=base_slug
             counter =1
             while Post.objects.filter(slug=slug).exists():
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index),
-    path('post/<slug:slug>/', views.post_detail),
+    path('', views.index, name='home'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
     path('about/', views.about),
 ]
